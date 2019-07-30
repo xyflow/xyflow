18baa3d4e3f1f1a11318b616320f8148d3bfa08d
moklick <info@moritzklack.com>
2019-07-30 13:39:42 +0200

    fix(nodeWrap): isHandle func

--- src/NodeRenderer/NodeTypes/wrapNode.js
+++ src/NodeRenderer/NodeTypes/wrapNode.js
@@ -8,7 +8,7 @@ import { isNode } from '../../graph-utils';
 import { Provider } from '../NodeIdContext';
 
 const isInput = e => ['INPUT', 'SELECT', 'TEXTAREA'].includes(e.target.nodeName);
-const isHandle = e => e.target.className.includes('source');
+const isHandle = e => e.target.className && e.target.className.includes('source');
 
 const getHandleBounds = (sel, nodeElement, parentBounds) => {
   const handle = nodeElement.querySelector(sel);
