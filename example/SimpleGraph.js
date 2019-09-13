5ce476ff818ab6639d8331ab4d25e3b411016c8c
moklick <info@moritzklack.com>
2019-09-13 16:25:12 +0200

    refactor(build): set env to production

--- example/SimpleGraph.js
+++ example/SimpleGraph.js
@@ -1,7 +1,7 @@
 import React, { PureComponent } from 'react';
 
 import Graph, { isEdge, removeElements, getOutgoers, Handle, MiniMap } from '../src';
-// import Graph, { isEdge, removeElements, getOutgoers, Handle } from '../dist/ReactGraph';
+// import Graph, { isEdge, removeElements, getOutgoers, Handle, MiniMap } from '../dist/ReactGraph';
 
 const SpecialNode = ({ data, styles }) => (
   <div
