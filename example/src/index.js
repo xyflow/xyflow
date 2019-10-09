73b28d8baf1ff8cf1a35b17988d4009669d86fd2
moklick <info@moritzklack.com>
2019-10-09 19:33:28 +0200

    refactor(example): use normal react dep

--- example/src/index.js
+++ example/src/index.js
@@ -1,4 +1,4 @@
-import React from '../../node_modules/react';
+import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 
