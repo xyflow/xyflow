63b83b2d63d87d6cc091b6f16d0dc6262fb28ad7
Moritz <info@moritzklack.com>
2020-05-25 14:00:56 +0200

    chore(selectionlistener): comment typo

--- src/components/SelectionListener/index.ts
+++ src/components/SelectionListener/index.ts
@@ -8,7 +8,7 @@ interface SelectionListenerProps {
 }
 
 // This is just a helper component for calling the onSelectionChange listener.
-// As soon as easy-peasy has implemented the effectOn hook, we can remove this compeonent
+// As soon as easy-peasy has implemented the effectOn hook, we can remove this component
 // and use the hook instead. https://github.com/ctrlplusb/easy-peasy/pull/459
 
 export default ({ onSelectionChange }: SelectionListenerProps) => {
