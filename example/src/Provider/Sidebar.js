e0ea040c1bfc0ca82ef7b6d8b9cf0de4e6d96dc8
moklick <info@moritzklack.com>
2020-06-03 21:35:19 +0200

    feat(example): add sidebar to provider example

---
+++ example/src/Provider/Sidebar.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import { useStoreState } from 'react-flow-renderer';
+
+export default () => {
+  const nodes = useStoreState((store) => store.nodes);
+  const transform = useStoreState((store) => store.transform);
+
+  return (
+    <aside>
+      <div className="description">
+        This is an example of how you can access the internal state outside of the ReactFlow component.
+      </div>
+      <div className="title">Zoom & pan transform</div>
+      <div className="transform">
+        [{transform[0].toFixed(2)}, {transform[1].toFixed(2)}, {transform[2].toFixed(2)}]
+      </div>
+      <div className="title">Nodes</div>
+      {nodes.map((node) => (
+        <div key={node.id}>
+          Node {node.id} - x: {node.__rg.position.x.toFixed(2)}, y: {node.__rg.position.y.toFixed(2)}
+        </div>
+      ))}
+    </aside>
+  );
+};
--- example/src/Provider/index.js
+++ example/src/Provider/index.js
@@ -1,8 +1,11 @@
 import React, { useState } from 'react';
+import ReactFlow, { ReactFlowProvider, addEdge, removeElements } from 'react-flow-renderer';
 
-import ReactFlow, { addEdge, ReactFlowProvider } from 'react-flow-renderer';
+import Sidebar from './Sidebar';
 
-const onElementClick = element => console.log('click', element);
+import './provider.css';
+
+const onElementClick = (element) => console.log('click', element);
 
 const initialElements = [
   { id: '1', type: 'input', data: { label: 'Node 1' }, position: { x: 250, y: 5 } },
@@ -10,22 +13,29 @@ const initialElements = [
   { id: '3', data: { label: 'Node 3' }, position: { x: 400, y: 100 } },
   { id: '4', data: { label: 'Node 4' }, position: { x: 400, y: 200 } },
   { id: 'e1-2', source: '1', target: '2', animated: true },
-  { id: 'e1-3', source: '1', target: '3' }
+  { id: 'e1-3', source: '1', target: '3' },
 ];
 
 const ProviderFlow = () => {
   const [elements, setElements] = useState(initialElements);
-  const onConnect = (params) => setElements(els => addEdge(params, els));
+  const onConnect = (params) => setElements((els) => addEdge(params, els));
+  const onElementsRemove = (elementsToRemove) => setElements((els) => removeElements(elementsToRemove, els));
 
   return (
-    <ReactFlowProvider>
-      <ReactFlow
-        elements={elements}
-        onElementClick={onElementClick}
-        onConnect={onConnect}
-      />
-    </ReactFlowProvider>
+    <div className="providerflow">
+      <ReactFlowProvider>
+        <Sidebar />
+        <div className="reactflow-wrapper">
+          <ReactFlow
+            elements={elements}
+            onElementClick={onElementClick}
+            onConnect={onConnect}
+            onElementsRemove={onElementsRemove}
+          />
+        </div>
+      </ReactFlowProvider>
+    </div>
   );
-}
+};
 
 export default ProviderFlow;
