f49cae85c05c2975575895773702e5cb74874376
moklick <info@moritzklack.com>
2020-12-22 12:09:35 +0100

    chore(examples): cleanup save restore

--- example/src/SaveRestore/index.js
+++ example/src/SaveRestore/index.js
@@ -1,63 +1,15 @@
-import React, { memo, useCallback, useState } from 'react';
-import localforage from 'localforage';
-import ReactFlow, { ReactFlowProvider, useZoomPanHelper, removeElements, addEdge } from 'react-flow-renderer';
+import React, { memo, useState } from 'react';
+import ReactFlow, { ReactFlowProvider, removeElements, addEdge } from 'react-flow-renderer';
+
+import Controls from './Controls';
 
 import './save.css';
-
-localforage.config({
-  name: 'react-flow',
-  storeName: 'flow',
-});
-
-const flowKey = 'save-restore-key';
 
 const initialElements = [
   { id: '1', data: { label: 'Node 1' }, position: { x: 100, y: 100 } },
   { id: '2', data: { label: 'Node 2' }, position: { x: 100, y: 200 } },
   { id: 'e1-2', source: '1', target: '2' },
 ];
-
-const getNodeId = () => `randomnode_${+new Date()}`;
-
-const Controls = memo(({ rfInstance, setElements }) => {
-  const { transform } = useZoomPanHelper();
-
-  const onSave = useCallback(() => {
-    const flow = rfInstance.toObject();
-    localforage.setItem(flowKey, flow);
-  }, [rfInstance]);
-
-  const onRestore = useCallback(() => {
-    const onRestoreFlow = async () => {
-      const flow = await localforage.getItem(flowKey);
-
-      if (flow) {
-        const [x = 0, y = 0] = flow.position;
-        setElements(flow.elements || []);
-        transform({ x, y, zoom: flow.zoom || 0 });
-      }
-    };
-
-    onRestoreFlow();
-  }, [setElements, transform]);
-
-  const onAdd = useCallback(() => {
-    const newNode = {
-      id: `random_node-${getNodeId()}`,
-      data: { label: 'Added node' },
-      position: { x: Math.random() * window.innerWidth - 100, y: Math.random() * window.innerHeight },
-    };
-    setElements((els) => els.concat(newNode));
-  }, [setElements]);
-
-  return (
-    <div className="save__controls">
-      <button onClick={onSave}>save</button>
-      <button onClick={onRestore}>restore</button>
-      <button onClick={onAdd}>add node</button>
-    </div>
-  );
-});
 
 const SaveRestore = () => {
   const [rfInstance, setRfInstance] = useState(null);
@@ -68,7 +20,7 @@ const SaveRestore = () => {
   return (
     <ReactFlowProvider>
       <ReactFlow elements={elements} onElementsRemove={onElementsRemove} onConnect={onConnect} onLoad={setRfInstance}>
-        {rfInstance && <Controls rfInstance={rfInstance} setElements={setElements} />}
+        <Controls rfInstance={rfInstance} setElements={setElements} />
       </ReactFlow>
     </ReactFlowProvider>
   );
