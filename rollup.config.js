09906c3f6e820999f7533c690066787e5dff9642
Christopher Möller <chrtze@gmail.com>
2021-02-25 12:17:05 +0100

    fix(build): remove babel require statements from build

--- rollup.config.js
+++ rollup.config.js
@@ -1,6 +1,6 @@
 import resolve from '@rollup/plugin-node-resolve';
 import commonjs from '@rollup/plugin-commonjs';
-import babel from '@rollup/plugin-babel';
+// import babel from '@rollup/plugin-babel';
 import postcss from 'rollup-plugin-postcss';
 import bundleSize from 'rollup-plugin-bundle-size';
 import replace from '@rollup/plugin-replace';
@@ -15,7 +15,7 @@ const processEnv = isProd || isTesting ? 'production' : 'development';
 
 export const baseConfig = ({ mainFile = pkg.main, moduleFile = pkg.module, injectCSS = true } = {}) => ({
   input: 'src/index.ts',
-  external: ['react', 'react-dom', /@babel\/runtime/],
+  external: ['react', 'react-dom'],
   onwarn(warning, rollupWarn) {
     if (warning.code !== 'CIRCULAR_DEPENDENCY') {
       rollupWarn(warning);
@@ -48,10 +48,10 @@ export const baseConfig = ({ mainFile = pkg.main, moduleFile = pkg.module, injec
       minimize: isProd,
       inject: injectCSS,
     }),
-    babel({
-      exclude: 'node_modules/**',
-      babelHelpers: 'runtime',
-    }),
+    // babel({
+    //   exclude: 'node_modules/**',
+    //   babelHelpers: 'runtime',
+    // }),
     svgr(),
     typescript({
       clean: true,
