95be228454903f333bf0ea7004fc09f7a2c525e9
moklick <info@moritzklack.com>
2021-02-25 13:35:39 +0100

    refactor(rollup): use babel only for cjs build

--- rollup.config.js
+++ rollup.config.js
@@ -1,11 +1,12 @@
 import resolve from '@rollup/plugin-node-resolve';
 import commonjs from '@rollup/plugin-commonjs';
-import babel from '@rollup/plugin-babel';
+import { getBabelOutputPlugin } from '@rollup/plugin-babel';
 import postcss from 'rollup-plugin-postcss';
 import bundleSize from 'rollup-plugin-bundle-size';
 import replace from '@rollup/plugin-replace';
 import svgr from '@svgr/rollup';
 import typescript from 'rollup-plugin-typescript2';
+import { DEFAULT_EXTENSIONS as DEFAULT_BABEL_EXTENSIONS } from '@babel/core';
 
 import pkg from './package.json';
 
@@ -27,6 +28,21 @@ export const baseConfig = ({ mainFile = pkg.main, moduleFile = pkg.module, injec
       format: 'cjs',
       sourcemap: true,
       exports: 'named',
+      plugins: [
+        getBabelOutputPlugin({
+          extensions: [...DEFAULT_BABEL_EXTENSIONS, 'ts', 'tsx'],
+          presets: [
+            [
+              '@babel/preset-env',
+              {
+                modules: false,
+              },
+            ],
+            '@babel/preset-react',
+          ],
+          plugins: ['@babel/transform-runtime'],
+        }),
+      ],
     },
     {
       file: moduleFile,
@@ -46,10 +62,7 @@ export const baseConfig = ({ mainFile = pkg.main, moduleFile = pkg.module, injec
       minimize: isProd,
       inject: injectCSS,
     }),
-    babel({
-      exclude: 'node_modules/**',
-      babelHelpers: 'runtime',
-    }),
+
     svgr(),
     resolve(),
     typescript({
