e8d26002a11103fad7f2be28743ff0178d75d548
moklick <info@moritzklack.com>
2021-10-25 16:56:54 +0200

    refactor(node-renderer): do not mutate node styles

--- src/container/NodeRenderer/index.tsx
+++ src/container/NodeRenderer/index.tsx
@@ -83,14 +83,14 @@ function Node({
   const childRect = useMemo(() => getRectOfNodes(childNodes), [childNodes]);
   const isParentNode = !!childNodes.length;
 
-  node.style = useMemo(() => {
+  const style = useMemo(() => {
     if (isParentNode) {
       return {
         ...node.style,
         width: Math.floor(childRect.width) + 20,
         height: Math.floor(childRect.height) + 20,
         boxSizing: 'border-box',
-      };
+      } as React.CSSProperties;
     }
     return node.style;
   }, [childRect.width, childRect.height, isParentNode, node.style]);
@@ -111,7 +111,7 @@ function Node({
     <NodeComponent
       id={node.id}
       className={node.className}
-      style={node.style}
+      style={style}
       type={nodeType}
       data={node.data}
       sourcePosition={node.sourcePosition}
