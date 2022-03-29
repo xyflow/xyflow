24f545aa618e36a1040994b6cf59b1eb4d249022
Rob Coenen <r.coenen@mendrix.nl>
2022-03-29 21:40:39 +0200

    replaced the very flakey useKeyPress implementation with useHotey to keep the (basic) functionality of useKeyPress but without the bugs. It won't support combo keys, but they didn't work properly before anyway. Using useHotkeys for combo's is much better and proven. A very light dependency.

--- src/hooks/useKeyPress.ts
+++ src/hooks/useKeyPress.ts
@@ -1,113 +1,28 @@
-import { useState, useEffect, useRef, useMemo } from 'react';
+import { useCallback, useMemo, useState } from 'react';
 
 import { KeyCode } from '../types';
+import { useHotkeys } from "react-hotkeys-hook";
 
-type Keys = Array<string>;
-type PressedKeys = Set<string>;
-type KeyOrCode = 'key' | 'code';
-export interface UseKeyPressOptions {
-  target: Window | Document | HTMLElement | ShadowRoot | null;
-}
-
-const doc = typeof document !== 'undefined' ? document : null;
-
-// the keycode can be a string 'a' or an array of strings ['a', 'a+d']
-// a string means a single key 'a' or a combination when '+' is used 'a+d'
-// an array means different possibilites. Explainer: ['a', 'd+s'] here the
-// user can use the single key 'a' or the combination 'd' + 's'
-export default (keyCode: KeyCode | null = null, options: UseKeyPressOptions = { target: doc }): boolean => {
+// the keycode can be a string 'a' or an array of strings ['a', 'd']
+// a string means a single key 'a' or 'Meta'
+// an array means different possibilities. Explainer: ['a', 'Shift'] here the
+// user can use the single key 'a' or the 'Shift' key or both to set the state to true.
+// If any of te pressed keys are released, te stat will return to false again, even if on of the keys is still held.
+// for combination hotkeys, like 'ctrl+a' use useHotkeys with a callback instead.
+export default (keyCode: KeyCode | null = null): boolean => {
   const [keyPressed, setKeyPressed] = useState(false);
+  const keyCodes = useMemo(() => Array.isArray(keyCode) ? keyCode : [keyCode || ''], [keyCode]);
+  const singles = useMemo(() => keyCodes.filter(k => !k.includes('+') || k === '+'), [keyCodes]);
 
-  // we need to remember the pressed keys in order to support combinations
-  const pressedKeys = useRef<PressedKeys>(new Set([]));
-
-  // keyCodes = array with single keys [['a']] or key combinations [['a', 's']]
-  // keysToWatch = array with all keys flattened ['a', 'd', 'ShiftLeft']
-  // used to check if we store event.code or event.key. When the code is in the list of keysToWatch
-  // we use the code otherwise the key. Explainer: When you press the left "command" key, the code is "MetaLeft"
-  // and the key is "Meta". We want users to be able to pass keys and codes so we assume that the key is meant when
-  // we can't find it in the list of keysToWatch.
-  const [keyCodes, keysToWatch] = useMemo<[Array<Keys>, Keys]>(() => {
-    if (keyCode !== null) {
-      const keyCodeArr = Array.isArray(keyCode) ? keyCode : [keyCode];
-      const keys = keyCodeArr.filter((kc) => typeof kc === 'string').map((kc) => kc.split('+'));
-      const keysFlat = keys.reduce((res: Keys, item) => res.concat(...item), []);
-
-      return [keys, keysFlat];
+  const handleSingleKeyEvent = useCallback((e: KeyboardEvent, isDown: boolean) => {
+    if (singles.includes(e.code) || singles.includes(e.key)) {
+      e.preventDefault();
+      setKeyPressed(isDown);
     }
+  },[singles, setKeyPressed]);
 
-    return [[], []];
-  }, [keyCode]);
-
-  useEffect(() => {
-    if (keyCode !== null) {
-      const downHandler = (event: KeyboardEvent) => {
-        const keyOrCode = useKeyOrCode(event.code, keysToWatch);
-        pressedKeys.current.add(event[keyOrCode]);
-
-        if (isMatchingKey(event, keyCodes, pressedKeys.current)) {
-          event.preventDefault();
-          setKeyPressed(true);
-        }
-      };
-
-      const upHandler = (event: KeyboardEvent) => {
-        const keyOrCode = useKeyOrCode(event.code, keysToWatch);
-
-        if (isMatchingKey(event, keyCodes, pressedKeys.current)) {
-          setKeyPressed(false);
-        }
-
-        pressedKeys.current.delete(event[keyOrCode]);
-      };
-
-      const resetHandler = () => {
-        pressedKeys.current.clear();
-        setKeyPressed(false);
-      };
-
-      options?.target?.addEventListener('keydown', downHandler as EventListenerOrEventListenerObject);
-      options?.target?.addEventListener('keyup', upHandler as EventListenerOrEventListenerObject);
-      window.addEventListener('blur', resetHandler);
-
-      return () => {
-        pressedKeys.current.clear();
-
-        options?.target?.removeEventListener('keydown', downHandler as EventListenerOrEventListenerObject);
-        options?.target?.removeEventListener('keyup', upHandler as EventListenerOrEventListenerObject);
-        window.removeEventListener('blur', resetHandler);
-      };
-    }
-  }, [keyCode, setKeyPressed]);
+  useHotkeys('*', (e) => handleSingleKeyEvent(e, true), {keydown: true});
+  useHotkeys('*', (e) => handleSingleKeyEvent(e, false), {keyup: true});
 
   return keyPressed;
 };
-
-// utils
-
-function isMatchingKey(event: KeyboardEvent, keyCodes: Array<Keys>, pressedKeys: PressedKeys): boolean {
-  if (isInputDOMNode(event)) {
-    return false;
-  }
-
-  return (
-    keyCodes
-      // we only want to compare same sizes of keyCode definitions
-      // and pressed keys. When the user specified 'Meta' as a key somewhere
-      // this would also be truthy without this filter when user presses 'Meta' + 'r'
-      .filter((keys) => keys.length === pressedKeys.size)
-      // since we want to support multiple possibilities only one of the
-      // combinations need to be part of the pressed keys
-      .some((keys) => keys.every((k) => pressedKeys.has(k)))
-  );
-}
-
-function useKeyOrCode(eventCode: string, keysToWatch: KeyCode): KeyOrCode {
-  return keysToWatch.includes(eventCode) ? 'code' : 'key';
-}
-
-function isInputDOMNode(e: KeyboardEvent): boolean {
-  const target = e?.target as HTMLElement;
-
-  return ['INPUT', 'SELECT', 'TEXTAREA'].includes(target?.nodeName) || target?.hasAttribute('contenteditable');
-}
