be89fe0020d9d0791b2dfb32d655923545e646a9
moklick <info@moritzklack.com>
2022-11-15 11:18:12 +0100

    refactor(edge-label-renderer): simplify selector

--- packages/core/src/components/EdgeLabelRenderer/index.tsx
+++ packages/core/src/components/EdgeLabelRenderer/index.tsx
@@ -3,12 +3,10 @@ import { createPortal } from 'react-dom';
 import { useStore } from '../../hooks/useStore';
 import { ReactFlowState } from '../../types';
 
-const selector = (s: ReactFlowState) => ({
-  edgeLabelRenderer: s.domNode?.querySelector('.react-flow__edgelabel-renderer'),
-});
+const selector = (s: ReactFlowState) => s.domNode?.querySelector('.react-flow__edgelabel-renderer');
 
 function EdgeLabelRenderer({ children }: { children: ReactNode }) {
-  const { edgeLabelRenderer } = useStore(selector);
+  const edgeLabelRenderer = useStore(selector);
 
   if (!edgeLabelRenderer) {
     return null;
