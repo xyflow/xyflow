4c35893e0f45744a484753b4345c09771282fd75
Christopher Möller <chrtze@gmail.com>
2022-12-06 18:08:51 +0100

    feat(node-toolbar): make nodeId optional and get id from context

--- packages/node-toolbar/src/types.ts
+++ packages/node-toolbar/src/types.ts
@@ -2,7 +2,7 @@ import { Position } from '@reactflow/core';
 import type { HTMLAttributes } from 'react';
 
 export type NodeToolbarProps = HTMLAttributes<HTMLDivElement> & {
-  nodeId: string | string[];
+  nodeId?: string | string[];
   isVisible?: boolean;
   position?: Position;
   offset?: number;
