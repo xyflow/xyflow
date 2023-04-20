70102b701b633760806692f54ef49bedf570f14a
braks <78412429+bcakmakoglu@users.noreply.github.com>
2023-04-20 19:07:53 +0200

    feat(core): allow array of ids as updateNodeInternals arg

--- packages/core/src/hooks/useUpdateNodeInternals.ts
+++ packages/core/src/hooks/useUpdateNodeInternals.ts
@@ -6,12 +6,16 @@ import type { UpdateNodeInternals } from '../types';
 function useUpdateNodeInternals(): UpdateNodeInternals {
   const store = useStoreApi();
 
-  return useCallback<UpdateNodeInternals>((id: string) => {
+  return useCallback<UpdateNodeInternals>((id: string | string[]) => {
     const { domNode, updateNodeDimensions } = store.getState();
     const nodeElement = domNode?.querySelector(`.react-flow__node[data-id="${id}"]`) as HTMLDivElement;
 
     if (nodeElement) {
-      requestAnimationFrame(() => updateNodeDimensions([{ id, nodeElement, forceUpdate: true }]));
+      const updateIds = Array.isArray(id) ? id : [id];
+
+      updateIds.forEach((id) => {
+        requestAnimationFrame(() => updateNodeDimensions([{ id, nodeElement, forceUpdate: true }]));
+      });
     }
   }, []);
 }
