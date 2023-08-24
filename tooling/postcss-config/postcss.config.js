a9376d9681403d0281c039c4ddd335ea57525b3b
moklick <info@moritzklack.com>
2023-08-24 13:58:40 +0200

    refactor(postcss): replace env hack with env file

--- tooling/postcss-config/postcss.config.js
+++ tooling/postcss-config/postcss.config.js
@@ -1,3 +1,5 @@
+require('dotenv').config();
+
 const postcssImport = require('postcss-import');
 const postcssNested = require('postcss-nested');
 const postcssCombine = require('postcss-combine-duplicated-selectors');
@@ -6,22 +8,16 @@ const autoprefixer = require('autoprefixer');
 const cssnano = require('cssnano');
 
 module.exports = (ctx) => {
-  // we pass the current lib (react or svelte) and the environment as an env param.
-  // When postcss is being used for the svelte examples, we are using this config,
-  // but we can't pass a custom env. That's why we need this little workaround.
-  const ctxEnv = ctx.env === 'development' ? 'svelte_development' : ctx.env;
-  const [lib, env] = ctxEnv.split('_');
-
   return {
     plugins: [
       postcssImport,
       postcssNested,
       postcssCombine,
       postcssRename({
-        strategy: (className) => className.replace('xy', lib),
+        strategy: (className) => className.replace('xy', process.env.LIB),
       }),
       autoprefixer,
-      env === 'production' && cssnano,
+      ctx.env === 'production' && cssnano,
     ],
   };
 };
