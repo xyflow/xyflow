969fa8280a913d470fab8a87c2aa917e9f2a3d29
Peter <p.korcsak@googlemail.com>
2023-10-04 10:37:39 +0200

    fix(svelte-flow) style is now applied properly to the connectionLine

--- packages/svelte/src/lib/components/ConnectionLine/ConnectionLine.svelte
+++ packages/svelte/src/lib/components/ConnectionLine/ConnectionLine.svelte
@@ -12,11 +12,11 @@
 
 {#if $connection.path}
   <svg width={$width} height={$height} class="svelte-flow__connectionline" style={containerStyle}>
-    <g class={cc(['svelte-flow__connection', $connection.status])} {style}>
+    <g class={cc(['svelte-flow__connection', $connection.status])}>
       <slot name="connectionLine" />
       <!-- slot fallbacks do not work if slots are forwarded in parent -->
       {#if !isCustomComponent}
-        <path d={$connection.path} fill="none" class="svelte-flow__connection-path" />
+        <path d={$connection.path} {style} fill="none" class="svelte-flow__connection-path" />
       {/if}
     </g>
   </svg>
