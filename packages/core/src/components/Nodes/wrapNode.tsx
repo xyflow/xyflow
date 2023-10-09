549c4dea169a892185e08850ce3c560aa352a99f
moklick <info@moritzklack.com>
2023-10-09 12:59:38 +0200

    fix(selection): select nodes on click when nodeDragThreshold > 0, closes #3487

--- packages/core/src/components/Nodes/wrapNode.tsx
+++ packages/core/src/components/Nodes/wrapNode.tsx
@@ -69,7 +69,9 @@ export default (NodeComponent: ComponentType<NodeProps>) => {
     const onContextMenuHandler = getMouseHandler(id, store.getState, onContextMenu);
     const onDoubleClickHandler = getMouseHandler(id, store.getState, onDoubleClick);
     const onSelectNodeHandler = (event: MouseEvent) => {
-      if (isSelectable && (!selectNodesOnDrag || !isDraggable)) {
+      const { nodeDragThreshold } = store.getState();
+
+      if (isSelectable && (!selectNodesOnDrag || !isDraggable || nodeDragThreshold > 0)) {
         // this handler gets called within the drag start event when selectNodesOnDrag=true
         handleNodeClick({
           id,
