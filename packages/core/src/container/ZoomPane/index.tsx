00e557f9fef0efbb82f578fa07eb7546a69d86c7
moklick <info@moritzklack.com>
2023-10-16 09:45:59 +0200

    fix(panOnScroll): use correct filter closes #3499

--- packages/core/src/container/ZoomPane/index.tsx
+++ packages/core/src/container/ZoomPane/index.tsx
@@ -345,7 +345,7 @@ const ZoomPane = ({
         // if the target element is inside an element with the nopan class, we prevent panning
         if (
           isWrappedWithClass(event, noPanClassName) &&
-          ((!panOnScroll && event.type !== 'wheel') || (panOnScroll && event.type === 'wheel'))
+          (event.type !== 'wheel' || (panOnScroll && event.type === 'wheel'))
         ) {
           return false;
         }
