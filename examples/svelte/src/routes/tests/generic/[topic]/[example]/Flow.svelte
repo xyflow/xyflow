00ff8e877d14bf1dd51d2fddab4f7c48b585a32f
Peter <p.korcsak@googlemail.com>
2023-11-07 16:43:02 +0100

    fixed svelte tests

--- examples/svelte/src/routes/tests/generic/[topic]/[example]/Flow.svelte
+++ examples/svelte/src/routes/tests/generic/[topic]/[example]/Flow.svelte
@@ -7,10 +7,10 @@
 	export let generic: GenericTestCase;
 
 	// Create writables here so it is easier to create test cases
-	const nodes = writable(generic.svelteFlowProps.nodes);
-	const edges = writable(generic.svelteFlowProps.edges);
+	const nodes = writable(generic.flowProps.nodes);
+	const edges = writable(generic.flowProps.edges);
 
-	const svelteFlowPropsWithWritables = { ...generic.svelteFlowProps, nodes, edges };
+	const svelteFlowPropsWithWritables = { ...generic.flowProps, nodes, edges };
 </script>
 
 <SvelteFlow {...svelteFlowPropsWithWritables} />
