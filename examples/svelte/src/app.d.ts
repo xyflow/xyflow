6db6d957d3c92cfc6688a14fdf1bf519c3819901
Peter <p.korcsak@googlemail.com>
2023-11-13 11:36:59 +0100

    chore(tests) added README and added plugins to generic tests

--- examples/svelte/src/app.d.ts
+++ examples/svelte/src/app.d.ts
@@ -1,6 +1,14 @@
 // See https://kit.svelte.dev/docs/types#app
 
-import type { Edge, Node, SvelteFlowProps } from '@xyflow/svelte';
+import type {
+	BackgroundProps,
+	Edge,
+	MiniMap,
+	MiniMapProps,
+	Node,
+	PanelProps,
+	SvelteFlowProps
+} from '@xyflow/svelte';
 
 // for information about these interfaces
 declare global {
@@ -13,6 +21,10 @@ declare global {
 
 	interface FlowConfig {
 		flowProps: Omit<SvelteFlowProps, 'nodes' | 'edges'> & { nodes: Node[]; edges: Edge[] };
+		panelProps: PanelProps;
+		backgroundProps: BackgroundProps;
+		controlsProps: ControlsProps;
+		minimapProps: MiniMapProps;
 	}
 }
 
--- examples/svelte/src/routes/tests/generic/[topic]/[example]/Flow.svelte
+++ examples/svelte/src/routes/tests/generic/[topic]/[example]/Flow.svelte
@@ -1,6 +1,6 @@
 <script lang="ts">
 	import { writable } from 'svelte/store';
-	import { SvelteFlow } from '@xyflow/svelte';
+	import { Background, Controls, MiniMap, Panel, SvelteFlow } from '@xyflow/svelte';
 
 	import '@xyflow/svelte/dist/style.css';
 
@@ -10,7 +10,20 @@
 	const nodes = writable(flowConfig.flowProps.nodes);
 	const edges = writable(flowConfig.flowProps.edges);
 
-	const props = { ...flowConfig.flowProps, nodes, edges };
+	const flowProps = { ...flowConfig.flowProps, nodes, edges };
 </script>
 
-<SvelteFlow {...props} />
+<SvelteFlow {...flowProps}>
+	{#if flowConfig.panelProps}
+		<Panel {...flowConfig.panelProps} />
+	{/if}
+	{#if flowConfig.minimapProps}
+		<MiniMap {...flowConfig.minimapProps} />
+	{/if}
+	{#if flowConfig.controlsProps}
+		<Controls {...flowConfig.controlsProps} />
+	{/if}
+	{#if flowConfig.backgroundProps}
+		<Background {...flowConfig.backgroundProps} />
+	{/if}
+</SvelteFlow>
