dab60f4a9e41d3b31c2eb7c5f90d16d58c07e3ae
moklick <info@moritzklack.com>
2023-11-23 12:15:15 +0100

    chore(examples): cleanup

--- examples/react/src/main.tsx
+++ examples/react/src/main.tsx
@@ -3,7 +3,7 @@ import { createRoot } from 'react-dom/client';
 
 import App from './App/index';
 
-import '@xyflow/react/dist/base.css';
+import '@xyflow/react/dist/style.css';
 import './index.css';
 
 createRoot(document.getElementById('root') as HTMLElement).render(
