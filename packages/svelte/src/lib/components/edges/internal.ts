f871617abeac66635d2ba4c3036e68802426a8c5
Peter <p.korcsak@googlemail.com>
2023-11-28 14:35:18 +0100

    renamed internal files in svelte for better alphabetical ordering

---
+++ packages/svelte/src/lib/components/edges/internal.ts
@@ -0,0 +1,4 @@
+export { default as BezierEdge } from './BezierEdgeInternal.svelte';
+export { default as StraightEdge } from './StraightEdgeInternal.svelte';
+export { default as SmoothStepEdge } from './SmoothStepEdgeInternal.svelte';
+export { default as StepEdge } from './StepEdgeInternal.svelte';
--- packages/svelte/src/lib/store/initial-store.ts
+++ packages/svelte/src/lib/store/initial-store.ts
@@ -24,10 +24,9 @@ import DefaultNode from '$lib/components/nodes/DefaultNode.svelte';
 import InputNode from '$lib/components/nodes/InputNode.svelte';
 import OutputNode from '$lib/components/nodes/OutputNode.svelte';
 import GroupNode from '$lib/components/nodes/GroupNode.svelte';
-import BezierEdge from '$lib/components/edges/InternalBezierEdge.svelte';
-import StraightEdge from '$lib/components/edges/InternalStraightEdge.svelte';
-import SmoothStepEdge from '$lib/components/edges/InternalSmoothStepEdge.svelte';
-import StepEdge from '$lib/components/edges/InternalStepEdge.svelte';
+
+import { BezierEdge, StraightEdge, StepEdge, SmoothStepEdge } from '$lib/components/edges/internal';
+
 import type {
   NodeTypes,
   EdgeTypes,
