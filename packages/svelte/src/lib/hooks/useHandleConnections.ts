99fc00f5dcc3b4c31d74e928f507b79991e873c5
peterkogo <peter@xyflow.com>
2024-02-15 11:32:55 +0100

    aligned useHandleConnections in svelte with react version

--- packages/svelte/src/lib/hooks/useHandleConnections.ts
+++ packages/svelte/src/lib/hooks/useHandleConnections.ts
@@ -2,10 +2,11 @@ import { derived } from 'svelte/store';
 import { areConnectionMapsEqual, type HandleConnection, type HandleType } from '@xyflow/system';
 
 import { useStore } from '$lib/store';
+import { getContext } from 'svelte';
 
 export type useHandleConnectionsParams = {
-  nodeId: string;
   type: HandleType;
+  nodeId?: string;
   id?: string | null;
 };
 
@@ -20,14 +21,18 @@ const initialConnections: HandleConnection[] = [];
  * @param param.id - the handle id (this is only needed if the node has multiple handles of the same type)
  * @returns an array with connections
  */
-export function useHandleConnections({ nodeId, type, id = null }: useHandleConnectionsParams) {
+export function useHandleConnections({ type, nodeId, id = null }: useHandleConnectionsParams) {
   const { edges, connectionLookup } = useStore();
+
+  const _nodeId = getContext<string>('svelteflow__node_id');
+  const currentNodeId = nodeId ?? _nodeId;
+
   let prevConnections: Map<string, HandleConnection> | undefined = undefined;
 
   return derived(
     [edges, connectionLookup],
     ([, connectionLookup], set) => {
-      const nextConnections = connectionLookup.get(`${nodeId}-${type}-${id || null}`);
+      const nextConnections = connectionLookup.get(`${currentNodeId}-${type}-${id || null}`);
 
       if (!areConnectionMapsEqual(nextConnections, prevConnections)) {
         prevConnections = nextConnections;
