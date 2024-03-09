7104872a12ab2840c4ba545fe6a23de06ee192a0
saswata <125185423+saswatax@users.noreply.github.com>
2024-03-09 15:43:37 +0530

    fix(useNodesData): fix crash when passing invalid node id

--- packages/system/src/utils/shallow-node-data.ts
+++ packages/system/src/utils/shallow-node-data.ts
@@ -2,7 +2,11 @@ import { NodeBase } from '../types';
 
 type NodeData = Pick<NodeBase, 'id' | 'type' | 'data'>;
 
-export function shallowNodeData(a: NodeData | NodeData[], b: NodeData | NodeData[]) {
+export function shallowNodeData(a: NodeData | null | NodeData[], b: NodeData | null | NodeData[]) {
+  if (a === null || b === null) {
+    return false;
+  }
+
   const _a = Array.isArray(a) ? a : [a];
   const _b = Array.isArray(b) ? b : [b];
 
