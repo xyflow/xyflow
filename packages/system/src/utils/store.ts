94c0ae71c33eb99685c8fb0ce6655c669b3dc9fb
Steven Roussey <sroussey@gmail.com>
2024-04-01 16:03:48 -0700

    fix: if this is set, nothing can be lower

--- packages/system/src/utils/store.ts
+++ packages/system/src/utils/store.ts
@@ -38,7 +38,7 @@ export function updateAbsolutePositions<NodeType extends NodeBase>(
         nodeLookup,
         {
           ...node.position,
-          z: node[internalsSymbol]?.z ?? 0,
+          z: 0,
         },
         parentNode?.origin || nodeOrigin
       );
