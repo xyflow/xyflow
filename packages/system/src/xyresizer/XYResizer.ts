6494c3fac4ccde8b5ab0e3b8cf4e3c85606d6a06
Tony Francis <tonyfrancisv@gmail.com>
2024-04-18 02:51:46 -0400

    call `onEnd` in `end` drag event hanlder in XYResizer

--- packages/system/src/xyresizer/XYResizer.ts
+++ packages/system/src/xyresizer/XYResizer.ts
@@ -89,7 +89,7 @@ function nodeToChildExtent(child: NodeBase, parent: NodeBase, nodeOrigin: NodeOr
   ];
 }
 
-export function XYResizer({ domNode, nodeId, getStoreItems, onChange }: XYResizerParams): XYResizerInstance {
+export function XYResizer({ domNode, nodeId, getStoreItems, onChange, onEnd }: XYResizerParams): XYResizerInstance {
   const selection = select(domNode);
 
   function update({
@@ -272,6 +272,7 @@ export function XYResizer({ domNode, nodeId, getStoreItems, onChange }: XYResize
       })
       .on('end', (event: ResizeDragEvent) => {
         onResizeEnd?.(event, { ...prevValues });
+        onEnd?.();
       });
     selection.call(dragHandler);
   }
