4994e94eb15e073f07907dfda4c0b47e42fb7803
peterkogo <peter@xyflow.com>
2024-05-15 12:28:24 +0200

    add default edge options

--- packages/svelte/src/lib/container/EdgeRenderer/EdgeRenderer.svelte
+++ packages/svelte/src/lib/container/EdgeRenderer/EdgeRenderer.svelte
@@ -1,17 +1,9 @@
 <script lang="ts">
   import { MarkerDefinition } from '$lib/container/EdgeRenderer/MarkerDefinition';
-  import { useStore } from '$lib/store';
   import type { EdgeRendererProps } from './types';
   import EdgeUpdater from '$lib/components/EdgeWrapper/EdgeUpdater.svelte';
 
   let { defaultEdgeOptions, onedgeclick, onedgecontextmenu, edges }: EdgeRendererProps = $props();
-
-  const store = useStore();
-  // TODO: Double check this
-  // if (defaultEdgeOptions) store.edges.setDefaultOptions(defaultEdgeOptions);
-  // $effect.pre(() => {
-  //   if (defaultEdgeOptions) store.edges.setDefaultOptions(defaultEdgeOptions);
-  // });
 </script>
 
 <div class="svelte-flow__edges">
@@ -20,6 +12,6 @@
   </svg>
 
   {#each edges as edge (edge.id)}
-    <EdgeUpdater id={edge.id} {edge} {onedgeclick} {onedgecontextmenu} />
+    <EdgeUpdater id={edge.id} {...defaultEdgeOptions} {edge} {onedgeclick} {onedgecontextmenu} />
   {/each}
 </div>
