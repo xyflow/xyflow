d5fefb5a6025d934db30a90f0ce63fbff4cd122f
peterkogo <peter@xyflow.com>
2024-06-04 09:16:35 +0200

    fixed panel overriding selection rect

--- packages/svelte/src/lib/container/Panel/Panel.svelte
+++ packages/svelte/src/lib/container/Panel/Panel.svelte
@@ -1,6 +1,7 @@
 <script lang="ts">
   import cc from 'classcat';
   import type { PanelProps } from './types';
+  import { useStore } from '$lib/store';
 
   let {
     position = 'top-right',
@@ -11,9 +12,16 @@
   }: PanelProps = $props();
 
   let positionClasses = $derived(`${position}`.split('-'));
+
+  let store = useStore();
 </script>
 
-<div class={cc(['svelte-flow__panel', className, ...positionClasses])} {style} {...restProps}>
+<div
+  class={cc(['svelte-flow__panel', className, ...positionClasses])}
+  {style}
+  style:pointer-events={store.selectionRectMode ? 'none' : undefined}
+  {...restProps}
+>
   {#if children}
     {@render children()}
   {/if}
