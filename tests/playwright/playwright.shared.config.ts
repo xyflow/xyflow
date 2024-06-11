a4f417b963b0e12fa1894fe46c57463a71f42b7c
moklick <info@moritzklack.com>
2024-06-11 15:58:20 +0200

    chore(tests): use list reporter

--- tests/playwright/playwright.shared.config.ts
+++ tests/playwright/playwright.shared.config.ts
@@ -17,7 +17,7 @@ export function sharedConfigWithPort({ port, framework }: ConfigParams): Playwri
     /* Opt out of parallel tests on CI. */
     workers: process.env.CI ? 1 : undefined,
     /* Reporter to use. See https://playwright.dev/docs/test-reporters */
-    reporter: 'html',
+    reporter: process.env.CI ? 'dot' : 'list',
     /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
     use: {
       /* Base URL to use in actions like `await page.goto('/')`. */
