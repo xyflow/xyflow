44ce37a0817849b7af27e38e8748ac15b71f8453
peterkogo <peter@xyflow.com>
2024-11-13 17:37:51 +0100

    fix preprocessor

--- packages/svelte/svelte.config.js
+++ packages/svelte/svelte.config.js
@@ -1,9 +1,9 @@
 import adapter from '@sveltejs/adapter-auto';
-import preprocess from 'svelte-preprocess';
+import { sveltePreprocess } from 'svelte-preprocess';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-  preprocess: preprocess(),
+  preprocess: sveltePreprocess(),
   kit: {
     adapter: adapter()
   }
