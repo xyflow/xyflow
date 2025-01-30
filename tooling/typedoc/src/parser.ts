4741fa2d6193e0f5712552e9af27ebc48459e58f
peterkogo <peter@xyflow.com>
2025-01-30 12:52:17 +0100

    use tsc

--- tooling/typedoc/src/parser.ts
+++ tooling/typedoc/src/parser.ts
@@ -2,6 +2,8 @@ import { writeOutputJSON } from './utils.js';
 import { ProjectParser } from 'typedoc-json-parser';
 
 export class Parser {
+  project: ProjectParser;
+
   constructor(typedocJSON) {
     this.project = new ProjectParser({ data: typedocJSON, dependencies: {} });
 
@@ -104,7 +106,31 @@ export class Parser {
   }
 
   async parseHook(item) {
-    await writeOutputJSON(`hooks/${item.name}.json`, item);
+    const name = item.name;
+    const source = item.source.url;
+
+    if (item.signatures.length > 1) {
+      console.warn(`Multiple signatures found for hook ${name}`);
+      // TODO: Overload Signatures
+    }
+
+    const signature = item.signatures[0];
+
+    const description = signature.comment.description;
+    const examples = signature.comment.example;
+
+    // Parsing the props
+    const hookParameters = [];
+    const parameters = signature.parameters;
+
+    const output = {
+      name,
+      source,
+      description,
+      examples,
+      item,
+    };
+    await writeOutputJSON(`hooks/${item.name}.json`, output);
   }
 
   async parseUtilFunction(item) {
