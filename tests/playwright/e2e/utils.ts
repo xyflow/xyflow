63847eded7ef8030bb4f8ff1315680eecac65d06
peterkogo <peter@xyflow.com>
2025-06-10 12:58:34 +0200

    fix pane tests

--- tests/playwright/e2e/utils.ts
+++ tests/playwright/e2e/utils.ts
@@ -3,14 +3,14 @@ const MATCH_ALL_NUMBERS = /[\d\.]+/g;
 // Type "Locator" not exported...
 export async function getTransform(element) {
   const transformString = await element.evaluate((el) => {
-    return el.style.transform;
+    return window.getComputedStyle(el).transform || '';
   });
 
-  // Parses all numbers in f.ex "translate(590px, 324px) scale(2)""
+  // Parses all numbers in "matrix(3.32571, 0, 0, 3.32571, 390.571, 133.851)"
   const transforms = transformString.match(MATCH_ALL_NUMBERS);
   return {
-    translateX: parseFloat(transforms![0]),
-    translateY: parseFloat(transforms![1]),
-    scale: parseFloat(transforms![2]),
+    translateX: parseFloat(transforms![4]),
+    translateY: parseFloat(transforms![5]),
+    scale: parseFloat(transforms![0]),
   };
 }
