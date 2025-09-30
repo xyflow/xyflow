321ebd2b10a247ae320414229000fa7346f2d94a
Vasuki Vardhan G <vasuki.v@melento.com>
2025-09-30 10:53:40 +0530

    chore: added playwright test cases

---
+++ examples/angular/src/app/app.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+
+@Component({
+  selector: 'app-root',
+  standalone: true,
+  imports: [RouterOutlet],
+  template: `
+    <router-outlet></router-outlet>
+  `
+})
+export class AppComponent {}
