4211ae1653190ba4a32baa4254d880aea1ff2516
Dima <dima@inkeep.com>
2026-04-06 02:28:24 +0700

    upd

--- packages/react/src/components/Edges/SmoothStepEdge.tsx
+++ packages/react/src/components/Edges/SmoothStepEdge.tsx
@@ -1,67 +1,65 @@
-import { memo } from 'react';
+/* eslint-disable react/prop-types */
+import { FC } from 'react';
 import { Position, getSmoothStepPath } from '@xyflow/system';
 
 import { BaseEdge } from './BaseEdge';
 import type { SmoothStepEdgeProps } from '../../types';
 
-function createSmoothStepEdge(params: { isInternal: boolean }) {
-  // eslint-disable-next-line react/display-name
-  return memo(
-    ({
-      id,
+const createSmoothStepEdge = (params: { isInternal: boolean }): FC<SmoothStepEdgeProps> =>
+  function MyComponent({
+    id,
+    sourceX,
+    sourceY,
+    targetX,
+    targetY,
+    label,
+    labelStyle,
+    labelShowBg,
+    labelBgStyle,
+    labelBgPadding,
+    labelBgBorderRadius,
+    style,
+    sourcePosition = Position.Bottom,
+    targetPosition = Position.Top,
+    markerEnd,
+    markerStart,
+    pathOptions,
+    interactionWidth,
+  }) {
+    'use memo';
+    const [path, labelX, labelY] = getSmoothStepPath({
       sourceX,
       sourceY,
+      sourcePosition,
       targetX,
       targetY,
-      label,
-      labelStyle,
-      labelShowBg,
-      labelBgStyle,
-      labelBgPadding,
-      labelBgBorderRadius,
-      style,
-      sourcePosition = Position.Bottom,
-      targetPosition = Position.Top,
-      markerEnd,
-      markerStart,
-      pathOptions,
-      interactionWidth,
-    }: SmoothStepEdgeProps) => {
-      const [path, labelX, labelY] = getSmoothStepPath({
-        sourceX,
-        sourceY,
-        sourcePosition,
-        targetX,
-        targetY,
-        targetPosition,
-        borderRadius: pathOptions?.borderRadius,
-        offset: pathOptions?.offset,
-        stepPosition: pathOptions?.stepPosition,
-      });
+      targetPosition,
+      borderRadius: pathOptions?.borderRadius,
+      offset: pathOptions?.offset,
+      stepPosition: pathOptions?.stepPosition,
+    });
 
-      const _id = params.isInternal ? undefined : id;
+    const _id = params.isInternal ? undefined : id;
 
-      return (
-        <BaseEdge
-          id={_id}
-          path={path}
-          labelX={labelX}
-          labelY={labelY}
-          label={label}
-          labelStyle={labelStyle}
-          labelShowBg={labelShowBg}
-          labelBgStyle={labelBgStyle}
-          labelBgPadding={labelBgPadding}
-          labelBgBorderRadius={labelBgBorderRadius}
-          style={style}
-          markerEnd={markerEnd}
-          markerStart={markerStart}
-          interactionWidth={interactionWidth}
-        />
-      );
-    }
-  );
-}
+    return (
+      <BaseEdge
+        id={_id}
+        path={path}
+        labelX={labelX}
+        labelY={labelY}
+        label={label}
+        labelStyle={labelStyle}
+        labelShowBg={labelShowBg}
+        labelBgStyle={labelBgStyle}
+        labelBgPadding={labelBgPadding}
+        labelBgBorderRadius={labelBgBorderRadius}
+        style={style}
+        markerEnd={markerEnd}
+        markerStart={markerStart}
+        interactionWidth={interactionWidth}
+      />
+    );
+  };
 
 /**
  * Component that can be used inside a custom edge to render a smooth step edge.
@@ -87,13 +85,11 @@ function createSmoothStepEdge(params: { isInternal: boolean }) {
  * ```
  */
 const SmoothStepEdge = createSmoothStepEdge({ isInternal: false });
-
+SmoothStepEdge.displayName = 'SmoothStepEdge';
 /**
  * @internal
  */
 const SmoothStepEdgeInternal = createSmoothStepEdge({ isInternal: true });
-
-SmoothStepEdge.displayName = 'SmoothStepEdge';
 SmoothStepEdgeInternal.displayName = 'SmoothStepEdgeInternal';
 
 export { SmoothStepEdge, SmoothStepEdgeInternal };
