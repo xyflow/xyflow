a3c22c468fe3ee1e50d76494aaf78fa2280203e2
Dima <dima@inkeep.com>
2026-04-06 00:55:30 +0700

    upd

--- packages/react/src/hooks/useViewportHelper.ts
+++ packages/react/src/hooks/useViewportHelper.ts
@@ -1,4 +1,3 @@
-import { useMemo } from 'react';
 import {
   pointToRendererPoint,
   getViewportForBounds,
@@ -19,105 +18,100 @@ import type { ViewportHelperFunctions } from '../types';
 const useViewportHelper = (): ViewportHelperFunctions => {
   const store = useStoreApi();
 
-  return useMemo<ViewportHelperFunctions>(() => {
-    return {
-      zoomIn: (options) => {
-        const { panZoom } = store.getState();
+  return {
+    zoomIn: (options) => {
+      const { panZoom } = store.getState();
 
-        return panZoom ? panZoom.scaleBy(1.2, options) : Promise.resolve(false);
-      },
-      zoomOut: (options) => {
-        const { panZoom } = store.getState();
+      return panZoom ? panZoom.scaleBy(1.2, options) : Promise.resolve(false);
+    },
+    zoomOut: (options) => {
+      const { panZoom } = store.getState();
 
-        return panZoom ? panZoom.scaleBy(1 / 1.2, options) : Promise.resolve(false);
-      },
-      zoomTo: (zoomLevel, options) => {
-        const { panZoom } = store.getState();
+      return panZoom ? panZoom.scaleBy(1 / 1.2, options) : Promise.resolve(false);
+    },
+    zoomTo: (zoomLevel, options) => {
+      const { panZoom } = store.getState();
 
-        return panZoom ? panZoom.scaleTo(zoomLevel, options) : Promise.resolve(false);
-      },
-      getZoom: () => store.getState().transform[2],
-      setViewport: async (viewport, options) => {
-        const {
-          transform: [tX, tY, tZoom],
-          panZoom,
-        } = store.getState();
+      return panZoom ? panZoom.scaleTo(zoomLevel, options) : Promise.resolve(false);
+    },
+    getZoom: () => store.getState().transform[2],
+    setViewport: async (viewport, options) => {
+      const {
+        transform: [tX, tY, tZoom],
+        panZoom,
+      } = store.getState();
 
-        if (!panZoom) {
-          return Promise.resolve(false);
-        }
+      if (!panZoom) {
+        return Promise.resolve(false);
+      }
 
-        await panZoom.setViewport(
-          {
-            x: viewport.x ?? tX,
-            y: viewport.y ?? tY,
-            zoom: viewport.zoom ?? tZoom,
-          },
-          options
-        );
+      await panZoom.setViewport(
+        {
+          x: viewport.x ?? tX,
+          y: viewport.y ?? tY,
+          zoom: viewport.zoom ?? tZoom,
+        },
+        options
+      );
 
-        return Promise.resolve(true);
-      },
-      getViewport: () => {
-        const [x, y, zoom] = store.getState().transform;
-        return { x, y, zoom };
-      },
-      setCenter: async (x, y, options) => {
-        return store.getState().setCenter(x, y, options);
-      },
-      fitBounds: async (bounds, options) => {
-        const { width, height, minZoom, maxZoom, panZoom } = store.getState();
-        const viewport = getViewportForBounds(bounds, width, height, minZoom, maxZoom, options?.padding ?? 0.1);
+      return Promise.resolve(true);
+    },
+    getViewport: () => {
+      const [x, y, zoom] = store.getState().transform;
+      return { x, y, zoom };
+    },
+    setCenter: async (x, y, options) => {
+      return store.getState().setCenter(x, y, options);
+    },
+    fitBounds: async (bounds, options) => {
+      const { width, height, minZoom, maxZoom, panZoom } = store.getState();
+      const viewport = getViewportForBounds(bounds, width, height, minZoom, maxZoom, options?.padding ?? 0.1);
 
-        if (!panZoom) {
-          return Promise.resolve(false);
-        }
+      if (!panZoom) {
+        return Promise.resolve(false);
+      }
 
-        await panZoom.setViewport(viewport, {
-          duration: options?.duration,
-          ease: options?.ease,
-          interpolate: options?.interpolate,
-        });
+      await panZoom.setViewport(viewport, {
+        duration: options?.duration,
+        ease: options?.ease,
+        interpolate: options?.interpolate,
+      });
 
-        return Promise.resolve(true);
-      },
-      screenToFlowPosition: (
-        clientPosition: XYPosition,
-        options: { snapToGrid?: boolean; snapGrid?: SnapGrid } = {}
-      ) => {
-        const { transform, snapGrid, snapToGrid, domNode } = store.getState();
+      return Promise.resolve(true);
+    },
+    screenToFlowPosition: (clientPosition: XYPosition, options: { snapToGrid?: boolean; snapGrid?: SnapGrid } = {}) => {
+      const { transform, snapGrid, snapToGrid, domNode } = store.getState();
 
-        if (!domNode) {
-          return clientPosition;
-        }
+      if (!domNode) {
+        return clientPosition;
+      }
 
-        const { x: domX, y: domY } = domNode.getBoundingClientRect();
-        const correctedPosition = {
-          x: clientPosition.x - domX,
-          y: clientPosition.y - domY,
-        };
-        const _snapGrid = options.snapGrid ?? snapGrid;
-        const _snapToGrid = options.snapToGrid ?? snapToGrid;
+      const { x: domX, y: domY } = domNode.getBoundingClientRect();
+      const correctedPosition = {
+        x: clientPosition.x - domX,
+        y: clientPosition.y - domY,
+      };
+      const _snapGrid = options.snapGrid ?? snapGrid;
+      const _snapToGrid = options.snapToGrid ?? snapToGrid;
 
-        return pointToRendererPoint(correctedPosition, transform, _snapToGrid, _snapGrid);
-      },
-      flowToScreenPosition: (flowPosition: XYPosition) => {
-        const { transform, domNode } = store.getState();
+      return pointToRendererPoint(correctedPosition, transform, _snapToGrid, _snapGrid);
+    },
+    flowToScreenPosition: (flowPosition: XYPosition) => {
+      const { transform, domNode } = store.getState();
 
-        if (!domNode) {
-          return flowPosition;
-        }
+      if (!domNode) {
+        return flowPosition;
+      }
 
-        const { x: domX, y: domY } = domNode.getBoundingClientRect();
-        const rendererPosition = rendererPointToPoint(flowPosition, transform);
+      const { x: domX, y: domY } = domNode.getBoundingClientRect();
+      const rendererPosition = rendererPointToPoint(flowPosition, transform);
 
-        return {
-          x: rendererPosition.x + domX,
-          y: rendererPosition.y + domY,
-        };
-      },
-    };
-  }, []);
+      return {
+        x: rendererPosition.x + domX,
+        y: rendererPosition.y + domY,
+      };
+    },
+  };
 };
 
 export default useViewportHelper;
