c81375ba3361f4d183405e9c89e722c520bbc443
Dima <dima@inkeep.com>
2026-04-06 02:38:58 +0700

    upd

--- packages/react/src/components/Edges/StraightEdge.tsx
+++ packages/react/src/components/Edges/StraightEdge.tsx
@@ -1,54 +1,49 @@
-import { memo } from 'react';
+/* eslint-disable react/prop-types */
+import type { FC } from 'react';
 import { getStraightPath } from '@xyflow/system';
 
 import { BaseEdge } from './BaseEdge';
 import type { StraightEdgeProps } from '../../types';
 
-function createStraightEdge(params: { isInternal: boolean }) {
-  // eslint-disable-next-line react/display-name
-  return memo(
-    ({
-      id,
-      sourceX,
-      sourceY,
-      targetX,
-      targetY,
-      label,
-      labelStyle,
-      labelShowBg,
-      labelBgStyle,
-      labelBgPadding,
-      labelBgBorderRadius,
-      style,
-      markerEnd,
-      markerStart,
-      interactionWidth,
-    }: StraightEdgeProps) => {
-      const [path, labelX, labelY] = getStraightPath({ sourceX, sourceY, targetX, targetY });
-
-      const _id = params.isInternal ? undefined : id;
-
-      return (
-        <BaseEdge
-          id={_id}
-          path={path}
-          labelX={labelX}
-          labelY={labelY}
-          label={label}
-          labelStyle={labelStyle}
-          labelShowBg={labelShowBg}
-          labelBgStyle={labelBgStyle}
-          labelBgPadding={labelBgPadding}
-          labelBgBorderRadius={labelBgBorderRadius}
-          style={style}
-          markerEnd={markerEnd}
-          markerStart={markerStart}
-          interactionWidth={interactionWidth}
-        />
-      );
-    }
-  );
-}
+const createStraightEdge = (params: { isInternal: boolean }): FC<StraightEdgeProps> =>
+  function MyComponent({
+    id,
+    sourceX,
+    sourceY,
+    targetX,
+    targetY,
+    label,
+    labelStyle,
+    labelShowBg,
+    labelBgStyle,
+    labelBgPadding,
+    labelBgBorderRadius,
+    style,
+    markerEnd,
+    markerStart,
+    interactionWidth,
+  }) {
+    'use memo'; // Hint for the React Compiler to treat this as a component
+    const [path, labelX, labelY] = getStraightPath({ sourceX, sourceY, targetX, targetY });
+    return (
+      <BaseEdge
+        id={params.isInternal ? undefined : id}
+        path={path}
+        labelX={labelX}
+        labelY={labelY}
+        label={label}
+        labelStyle={labelStyle}
+        labelShowBg={labelShowBg}
+        labelBgStyle={labelBgStyle}
+        labelBgPadding={labelBgPadding}
+        labelBgBorderRadius={labelBgBorderRadius}
+        style={style}
+        markerEnd={markerEnd}
+        markerStart={markerStart}
+        interactionWidth={interactionWidth}
+      />
+    );
+  };
 
 /**
  * Component that can be used inside a custom edge to render a straight line.
@@ -72,13 +67,12 @@ function createStraightEdge(params: { isInternal: boolean }) {
  * ```
  */
 const StraightEdge = createStraightEdge({ isInternal: false });
+StraightEdge.displayName = 'StraightEdge';
 
 /**
  * @internal
  */
 const StraightEdgeInternal = createStraightEdge({ isInternal: true });
-
-StraightEdge.displayName = 'StraightEdge';
 StraightEdgeInternal.displayName = 'StraightEdgeInternal';
 
 export { StraightEdge, StraightEdgeInternal };
