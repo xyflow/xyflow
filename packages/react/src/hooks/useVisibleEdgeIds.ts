1eeecc5427da11b71984c46e96a26bc73b89908b
Dima <dima@inkeep.com>
2026-04-06 02:56:59 +0700

    upd

--- packages/react/src/hooks/useVisibleEdgeIds.ts
+++ packages/react/src/hooks/useVisibleEdgeIds.ts
@@ -1,9 +1,7 @@
-import { useCallback } from 'react';
 import { shallow } from 'zustand/shallow';
 import { isEdgeVisible } from '@xyflow/system';
 
 import { useStore } from './useStore';
-import { type ReactFlowState } from '../types';
 
 /**
  * Hook for getting the visible edge ids from the store.
@@ -13,42 +11,36 @@ import { type ReactFlowState } from '../types';
  * @returns array with visible edge ids
  */
 export function useVisibleEdgeIds(onlyRenderVisible: boolean): string[] {
-  const edgeIds = useStore(
-    useCallback(
-      (s: ReactFlowState) => {
-        if (!onlyRenderVisible) {
-          return s.edges.map((edge) => edge.id);
+  const edgeIds = useStore((s) => {
+    if (!onlyRenderVisible) {
+      return s.edges.map((edge) => edge.id);
+    }
+
+    const visibleEdgeIds = [];
+
+    if (s.width && s.height) {
+      for (const edge of s.edges) {
+        const sourceNode = s.nodeLookup.get(edge.source);
+        const targetNode = s.nodeLookup.get(edge.target);
+
+        if (
+          sourceNode &&
+          targetNode &&
+          isEdgeVisible({
+            sourceNode,
+            targetNode,
+            width: s.width,
+            height: s.height,
+            transform: s.transform,
+          })
+        ) {
+          visibleEdgeIds.push(edge.id);
         }
+      }
+    }
 
-        const visibleEdgeIds = [];
-
-        if (s.width && s.height) {
-          for (const edge of s.edges) {
-            const sourceNode = s.nodeLookup.get(edge.source);
-            const targetNode = s.nodeLookup.get(edge.target);
-
-            if (
-              sourceNode &&
-              targetNode &&
-              isEdgeVisible({
-                sourceNode,
-                targetNode,
-                width: s.width,
-                height: s.height,
-                transform: s.transform,
-              })
-            ) {
-              visibleEdgeIds.push(edge.id);
-            }
-          }
-        }
-
-        return visibleEdgeIds;
-      },
-      [onlyRenderVisible]
-    ),
-    shallow
-  );
+    return visibleEdgeIds;
+  }, shallow);
 
   return edgeIds;
 }
