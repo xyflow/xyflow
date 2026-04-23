87a527ef5cfc888866d549bcb4db4799f0caf8ab
peterkogo <peter@xyflow.com>
2026-04-23 13:39:05 +0200

    adopt official svelte package settings

--- packages/svelte/eslint.config.js
+++ packages/svelte/eslint.config.js
@@ -1,5 +1,5 @@
 import prettier from 'eslint-config-prettier';
-import { fileURLToPath } from 'node:url';
+import path from 'node:path';
 import { includeIgnoreFile } from '@eslint/compat';
 import js from '@eslint/js';
 import svelte from 'eslint-plugin-svelte';
@@ -8,19 +8,17 @@ import globals from 'globals';
 import ts from 'typescript-eslint';
 import svelteConfig from './svelte.config.js';
 
-const gitignorePath = fileURLToPath(new URL('./.gitignore', import.meta.url));
+const gitignorePath = path.resolve(import.meta.dirname, '.gitignore');
 
 export default defineConfig(
   includeIgnoreFile(gitignorePath),
   js.configs.recommended,
-  ...ts.configs.recommended,
-  ...svelte.configs.recommended,
+  ts.configs.recommended,
+  svelte.configs.recommended,
   prettier,
-  ...svelte.configs.prettier,
+  svelte.configs.prettier,
   {
-    languageOptions: {
-      globals: { ...globals.browser, ...globals.node }
-    },
+    languageOptions: { globals: { ...globals.browser, ...globals.node } },
     rules: {
       // typescript-eslint strongly recommend that you do not use the no-undef lint rule on TypeScript projects.
       // see: https://typescript-eslint.io/troubleshooting/faqs/eslint/#i-get-errors-from-the-no-undef-rule-about-global-variables-not-being-defined-even-though-there-are-no-typescript-errors
@@ -37,5 +35,10 @@ export default defineConfig(
         svelteConfig
       }
     }
+  },
+  {
+    // Override or add rule settings here, such as:
+    // 'svelte/button-has-type': 'error'
+    rules: {}
   }
 );
