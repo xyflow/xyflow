5ef39cb29ab477b93d143aa64d9b634eeeb97e57
peterkogo <peter@xyflow.com>
2026-05-12 10:30:52 +0200

    Remove unused css variables

--- packages/svelte/src/lib/container/SvelteFlow/Wrapper.svelte
+++ packages/svelte/src/lib/container/SvelteFlow/Wrapper.svelte
@@ -139,22 +139,5 @@
     overflow: hidden;
     position: relative;
     z-index: 0;
-
-    background-color: var(--background-color, var(--background-color-default));
-  }
-
-  :root {
-    --background-color-default: #fff;
-    --background-pattern-color-default: #ddd;
-
-    --minimap-mask-color-default: rgb(240, 240, 240, 0.6);
-    --minimap-mask-stroke-color-default: none;
-    --minimap-mask-stroke-width-default: 1;
-
-    --controls-button-background-color-default: #fefefe;
-    --controls-button-background-color-hover-default: #f4f4f4;
-    --controls-button-color-default: inherit;
-    --controls-button-color-hover-default: inherit;
-    --controls-button-border-color-default: #eee;
   }
 </style>
