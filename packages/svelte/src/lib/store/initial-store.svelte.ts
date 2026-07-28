dfcb21dd9b9390091f836741d416f7c5ea7c4228
peterkogo <peter@xyflow.com>
2026-07-28 13:12:32 +0200

    make 5% new default padding

--- packages/svelte/src/lib/store/initial-store.svelte.ts
+++ packages/svelte/src/lib/store/initial-store.svelte.ts
@@ -91,6 +91,9 @@ function getInitialViewport<NodeType extends Node = Node>(
   // This is just used to make sure adoptUserNodes is called before we calculate the viewport
   _nodesInitialized: boolean,
   fitView: boolean | undefined,
+  fitViewOptions: FitViewOptions<NodeType> | undefined,
+  minZoom: number = 0.5,
+  maxZoom: number = 2,
   initialViewport: Viewport | undefined,
   width: number,
   height: number,
@@ -100,7 +103,14 @@ function getInitialViewport<NodeType extends Node = Node>(
     const bounds = getInternalNodesBounds(nodeLookup, {
       filter: (node) => !!((node.width || node.initialWidth) && (node.height || node.initialHeight))
     });
-    return getViewportForBounds(bounds, width, height, 0.5, 2, 0.1);
+    return getViewportForBounds(
+      bounds,
+      width,
+      height,
+      minZoom,
+      maxZoom,
+      fitViewOptions?.padding ?? '5%'
+    );
   } else {
     return initialViewport ?? { x: 0, y: 0, zoom: 1 };
   }
@@ -335,6 +345,9 @@ export function getInitialStore<NodeType extends Node = Node, EdgeType extends E
       getInitialViewport(
         this.nodesInitialized,
         signals.props.fitView,
+        signals.props.fitViewOptions,
+        signals.props.minZoom,
+        signals.props.maxZoom,
         signals.props.initialViewport,
         this.width,
         this.height,
