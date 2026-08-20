c25d55ba8448b96dc6a1a7698897d37cac52067a
moklick <info@moritzklack.com>
2026-08-20 09:31:11 +0200

    chore(attr): update link

--- packages/system/src/utils/attribution.ts
+++ packages/system/src/utils/attribution.ts
@@ -12,7 +12,7 @@ export function handleAttributionWarning(library: 'react' | 'svelte' | 'vue') {
   setTimeout(() => {
     if (!isDomNodeVisible(`.${library}-flow__attribution`)) {
       console.warn(
-        `${framework}: It seems like you are hiding the attribution. Please only do this when you are subscribed to ${framework} Pro: https://${library}flow.dev/remove-attribution?utm_source=console\n%cYou can ignore this warning if you are subscribed.`,
+        `${framework}: It seems like you are hiding the attribution. Please only do this when you are subscribed to ${framework} Pro: https://${library}flow.dev/remove-attr\n%cYou can ignore this warning if you are subscribed.`,
         'font-style: italic;'
       );
     }
