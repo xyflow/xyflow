b5a114dd096b43160b62a21231c51932e2c7d1d6
jundejz <jundejz@gmail.com>
2026-08-21 15:10:42 +0100

    add tests

--- examples/react/src/generic-tests/Flow.tsx
+++ examples/react/src/generic-tests/Flow.tsx
@@ -2,6 +2,7 @@ import { useState, useCallback } from 'react';
 import {
   ReactFlow,
   addEdge,
+  reconnectEdge,
   OnNodesChange,
   OnEdgesChange,
   OnConnect,
@@ -11,6 +12,7 @@ import {
   Background,
   Node,
   Edge,
+  OnReconnect,
 } from '@xyflow/react';
 
 type FlowProps = {
@@ -25,10 +27,14 @@ export default ({ flowConfig }: FlowProps) => {
   const onNodesChange: OnNodesChange = useCallback((changes) => setNodes((nds) => changes.applyTo(nds as Node[])), []);
   const onEdgesChange: OnEdgesChange = useCallback((changes) => setEdges((eds) => changes.applyTo(eds as Edge[])), []);
   const onConnect: OnConnect = useCallback((params) => setEdges((eds) => addEdge(params, eds as Edge[])), []);
+  const onReconnect: OnReconnect = useCallback(
+    (oldEdge, connection) => setEdges((eds) => reconnectEdge(oldEdge, connection, eds)),
+    []
+  );
 
   return (
     <div style={{ height: '100%' }}>
-      <ReactFlow {...props} onNodesChange={onNodesChange} onEdgesChange={onEdgesChange} onConnect={onConnect}>
+      <ReactFlow {...props} onNodesChange={onNodesChange} onEdgesChange={onEdgesChange} onConnect={onConnect} onReconnect={onReconnect}>
         {flowConfig.controlsProps && <Controls {...flowConfig.controlsProps} />}
         {flowConfig.panelProps && <Panel {...flowConfig.panelProps} />}
         {flowConfig.minimapProps && <MiniMap {...flowConfig.minimapProps} />}
